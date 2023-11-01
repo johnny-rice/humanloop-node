d0afa4918c1f4e153a0865cea69a91b9e01ba539
eddiechayes <eddiechayes@gmail.com>
2023-11-01 11:39:10 -0700

    Regenerate and Version bump SDK

--- configuration.ts
+++ configuration.ts
@@ -164,7 +164,7 @@ export class Configuration {
         if (param.useFetch ?? true) {
             this.baseOptions["adapter"] = fetchAdapter
         }
-        this.userAgent = param.userAgent === undefined ? "Konfig/0.5.20/typescript" : param.userAgent;
+        this.userAgent = param.userAgent === undefined ? "Konfig/0.5.21/typescript" : param.userAgent;
         this.formDataCtor = param.formDataCtor;
     }
 
