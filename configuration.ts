ead6f167ac23479e49e1d9db332c57d27a82a121
konfig-publisher <publisher@konfigthis.com>
2024-01-25 17:31:29 +0000

    Version Bump SDKs

--- configuration.ts
+++ configuration.ts
@@ -164,7 +164,7 @@ export class Configuration {
         if (param.useFetch ?? true) {
             this.baseOptions["adapter"] = fetchAdapter
         }
-        this.userAgent = param.userAgent === undefined ? "Konfig/0.6.1/typescript" : param.userAgent;
+        this.userAgent = param.userAgent === undefined ? "Konfig/0.6.2/typescript" : param.userAgent;
         this.formDataCtor = param.formDataCtor;
     }
 
